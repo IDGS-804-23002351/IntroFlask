e89cb7725fc83b50a4cc88583cf4f6521605001d
Gabriela Paola Perez Ornelas <paolaornelas17232216@gmail.com>
2026-01-17 09:50:01 -0600

    operaciones basicas

---
+++ app.py
@@ -0,0 +1,68 @@
+#se realiza la importación de flask
+from flask import Flask, render_template,request
+#se coloca el nombre
+app = Flask(__name__)
+
+#este es un decorador
+@app.route('/')
+
+#se agrega la primera funcion de la ruta principal
+def home():
+    title = "Pagina - Intro Flask"
+    listado = ['Juan','Ana','Pedro','Luisa']
+    return render_template('index.html',title=title, listado=listado)
+@app.route('/saludo1')
+def saludo1():
+    return render_template('saludo1.html')
+@app.route('/saludo2')
+def saludo2():
+    return render_template('saludo2.html')
+#se agrega la segunda funcion de una ruta creada despues de la principal
+@app.route("/hola")
+def func():
+    return 'Hola'
+#se agrega la tercera funcion enviando datos string de una ruta creada despues de la principal
+@app.route("/user/<string:user>")
+def user(user):
+    return f'Hola, {user}'
+
+@app.route("/numero/<int:n>")
+def numero(n):
+    return f'<h1>Número:{n}</h1>'
+
+@app.route("/user/<int:id>/<string:username>")
+def username(id, username):
+    return f'<h1>Hola {username} y tu id es: {id}</h1>'
+
+@app.route("/suma/<float:n1>/<float:n2>")
+def suma(n1,n2):
+    return f'<h1>La suma es: {n1+n2}</h1>'
+
+@app.route("/default/")
+@app.route("/default/<string:parm>")
+def func2(param="juan"):
+    return f'<h1>Hola, {param}</h1>'
+
+@app.route("/operas")
+def operas():
+    return '''
+        <form>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+            <br>
+            <label for="name">apaterno:</label>
+            <input type="text" id="name" name="name" required>
+            <br>
+             <input type="submit" value="Submit">
+        </form>
+    '''
+@app.route("/operasBas")
+def operasBas():
+    return render_template('operasBas.html')
+@app.route("/resultado",methods=['GET','POST'])
+def resul1():
+    n1 = request.form.get('num1')
+    n2 = request.form.get('num2')
+    return f'<h1>La suma es: {float(n1) + float(n2)}</h1>'
+if __name__ == '__main__':
+    app.run(debug=True)
